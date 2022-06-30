77edeea49bb95a4cd2d1ec0b90735c9af9d5e2fb
Tomasz Murzynski <tomasz.murzynski@gmail.com>
2022-06-30 16:29:53 +0200

    Fix Dodawanie, odejmowanie, mnożenie dzielenie

--- Kalkulator/obj/Debug/net6.0-windows/App.g.i.cs
+++ Kalkulator/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Kalkulator {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Kalkulator;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Kalkulator;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
